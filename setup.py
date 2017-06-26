795029c22e822b0661a01a6c38bc5b17c9cd9bc2
Sante <hyrsky@riseup.net>
2017-06-27 02:06:15 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+import os
+from distutils.core import setup
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='Pankkiyhteys',
+    version='0.1',
+    description='Communicate with Finnish banks',
+    license='MIT License',
+    author='Santeri Hurnanen',
+    author_email='hyrsky@riseup.net',
+    url='https://github.com/hyrsky/Pankkiyhteys',
+    packages=['pankkiyhteys', 'pankkiyhteys.b2c', 'pankkiyhteys.finvoice'],
+)
